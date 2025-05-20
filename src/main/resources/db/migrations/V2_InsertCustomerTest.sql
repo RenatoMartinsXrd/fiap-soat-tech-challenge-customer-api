41f672a2724919e5281e3eafc8490a6159570b61
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-05-20 18:19:13 -0300

    Create V2_InsertCustomerTest.sql

---
+++ src/main/resources/db/migrations/V2_InsertCustomerTest.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer_entity (name, email, cpf)
+VALUES ('João Silva', 'joao@teste.com', '111.111.111-11');
+
+INSERT INTO customer_entity (name, email, cpf)
+VALUES ('Maria Souza', 'maria@teste.com', '222.222.222-22');
